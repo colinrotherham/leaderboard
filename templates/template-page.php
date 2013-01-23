3d65116c1778bea31a67c7877edaaff9674c96c3
Colin Rotherham <work@colinr.com>
2013-01-23 22:32:49 +0000

    Ensure page title and body class uses HTML::entities

--- templates/template-page.php
+++ templates/template-page.php
@@ -2,12 +2,13 @@
 	use \CRD\Core\App as App;
 	use \CRD\Core\Template as Template;
 	use \CRD\Core\Resource as Resource;
+	use \CRD\Core\HTML as HTML;
 
 ?><!doctype html>
 <html lang="<?php echo Resource::$locale; ?>">
 	<head>
 		<meta charset="utf-8">
-		<title><?= ((!empty(Template::$title))? Template::$title . ' — ' : '') . App::$name; ?></title>
+		<title><?= HTML::entities(((!empty(Template::$title))? Template::$title . ' — ' : '') . App::$name) ?></title>
 
 		<!-- Handheld support -->
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -18,7 +19,7 @@
 		<!-- Initialise advanced UI -->
 		<script>document.documentElement.className = 'wf-loading advanced';</script>
 	</head>
-	<body class="<?= Template::$name ?>">
+	<body class="<?= HTML::entities(Template::$name) ?>">
 	
 		<div id="container">
 <?= Template::content('main') ?>
