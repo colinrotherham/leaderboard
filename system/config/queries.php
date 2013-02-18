62f625feb9f417de038504d981cedda7df578c83
Colin Rotherham <work@colinr.com>
2013-02-18 09:29:57 +0000

    Remove copyright entry from query config

--- system/config/queries.php
+++ system/config/queries.php
@@ -1,9 +1,8 @@
 <?php
 
 /*
-	Copyright (c) 2012 Colin Rotherham, http://colinr.com
-	https://github.com/colinrotherham
-*/
+	MySQL query config
+	----------------------------------- */
 
 	namespace CRD\Core;
 
